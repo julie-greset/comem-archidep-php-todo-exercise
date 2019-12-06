9e00c28d3d459913a6b8e09663470a4a625666c4
julie-greset <56160733+julie-greset@users.noreply.github.com>
2019-12-06 15:53:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-define('BASE_URL', '/comem-archidep-php-todo-exercise/');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('BASE_URL', '/');
+define('DB_USER', 'todolist');
+define('DB_PASS', 'password-todolist-A!1');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '8889');
+define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
